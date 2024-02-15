17e4b288878d896a42c391b57a10d76cf3294886
Sadi Pavani <sadipavani21@gmail.com>
2024-02-15 21:27:03 +0530

    counter-component

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { useState } from "react";
 function App() {
+  const [value,setValue]=useState(0);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  <div className="box">
+    <div className="container">
+      <h1>React counter</h1>
+      <h2>{value}</h2>
+      <button className="btn" onClick={()=>{setValue(value+1)}}>Inc</button>
+      <button className="btn" onClick={()=>{setValue(value-1)}}>Dec</button>
+      <button className="btn" onClick={()=>{setValue(0)}}>Reset</button>
     </div>
+  </div> 
   );
 }
 
